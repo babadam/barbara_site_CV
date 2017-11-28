6555e02c893aa7d7b28f634617cf1ff43497574e
babadam <barbara.tousverts@lepoles.com>
2017-11-28 16:50:43 +0100

    commit n°24

---
+++ accueil_pro.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Site public - Barbara</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Francois+One|Josefin+Sans|Oswald" rel="stylesheet">
+    </head>
+    <body>
+        <nav>
+            <div class="listeNav">
+                <a href="#">Experiences</a>
+                <a href="#">Réalisations</a>
+                <a href="#">Formations</a>
+                <a href="#">Compétences</a>
+                <a href="#">Loisirs</a>
+                <a href="#">Réseaux</a>
+                <a href="#">Contact</a>
+            </div>
+        </nav>
+        <div id="slide1">
+            <div class="slide_inside">
+                <h1>Page front</h1>
+                <h2>Compétences</h2>
+            </div> <!--fin .slide_inside-->
+        </div> <!--fin #slide1-->
+
+        <div id="slide2">
+            <div class="slide_inside">
+                <h2>Expériences</h2>
+            </div> <!--.slide_inside-->
+        </div> <!--#slide2-->
+
+        <div id="slide3">
+            <div class="slide_inside">
+                <h2>Crédits</h2>
+            </div> <!--.slide_inside-->
+        </div>
+    </body>
+</html>
