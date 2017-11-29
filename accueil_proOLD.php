985a2da88ab3b8173c259ba4f7ab77a7ff9bc369
babadam <barbara.tousverts@lepoles.com>
2017-11-29 17:03:58 +0100

    commit n°25

--- accueil_proOLD.php
+++ accueil_proOLD.php
@@ -7,28 +7,28 @@
         <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Francois+One|Josefin+Sans|Oswald" rel="stylesheet">
     </head>
     <body>
-        <nav id="navigation" role="navigation" class="navigation">
-            <a href="#">Experiences</a>
-            <a href="#">Réalisations</a>
-            <a href="#">Formations</a>
-            <a href="#">Compétences</a>
-            <a href="#">Loisirs</a>
-            <a href="#">Réseaux</a>
-            <a href="#">Contact</a>
+        <nav>
+            <div class="listeNav">
+                <a href="#">Experiences</a>
+                <a href="#">Réalisations</a>
+                <a href="#">Formations</a>
+                <a href="#">Compétences</a>
+                <a href="#">Loisirs</a>
+                <a href="#">Réseaux</a>
+                <a href="#">Contact</a>
+            </div>
         </nav>
-        <div class="main" role="main">
-          <button class="nav-button" role="button" type="button" aria-label="navigation"></button>
-        </div>
         <div id="slide1">
             <div class="slide_inside">
-                <h1>Barbara Tousverts </h1>
-                <p><span id="holder"></span><span class="blinking-cursor">|</span></p>
+                <h1>Tousverts Barbara</h1>
+                <p>Recherche d'un stage de deux mois</p>
+                <p>Développement - Intégration Web</p>
             </div> <!--fin .slide_inside-->
         </div> <!--fin #slide1-->
 
         <div id="slide2">
             <div class="slide_inside">
-                <h2>Compétences</h2>
+                <h2>Expériences</h2>
             </div> <!--.slide_inside-->
         </div> <!--#slide2-->
 
@@ -37,8 +37,6 @@
                 <h2>Crédits</h2>
             </div> <!--.slide_inside-->
         </div>
-        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script src="js/main.js" type="text/javascript"></script>
-
     </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,14 +1,51 @@
-//var texte0 = "Mon texte....";
-//var blancs = " ";
-//texte0 += blancs;
-//var longueur = texte0.length;
-//var n = 0;
-//onEnterFrame = function() {
-//if(n != longueur) {
-//    var texte1 = texte0.substr(0, n); 
-//    affichage1.text = texte1;
-//    n++;
-//} 
-//else {delete this.onEnterFrame;}
-//}
 //alert('HI');
+
+// fonction menu burger
+(function() {
+
+// old browser or not ?
+if ( !('querySelector' in document && 'addEventListener' in window) ) {
+return;
+}
+window.document.documentElement.className += ' js-enabled';
+
+function toggleNav() {
+
+// Define targets
+var target = document.querySelector('.navigation');
+var button = document.querySelector('.nav-button');
+
+// click-touch event
+if ( button ) {
+  button.addEventListener('click',
+  function (e) {
+    target.classList.toggle('is-opened');
+    e.preventDefault();
+  }, false );
+}
+} // end toggleNav()
+
+toggleNav();
+}());
+
+
+$(document).ready(function() {
+
+  // typing animation
+  (function($) {
+    $.fn.writeText = function(content) {
+        var contentArray = content.split(""),
+            current = 0,
+            elem = this;
+        setInterval(function() {
+            if(current < contentArray.length) {
+                elem.text(elem.text() + contentArray[current++]);
+            }
+        }, 80);
+    };
+
+  })(jQuery);
+
+  // input text for typing animation
+  $("#holder").writeText("Développeuse - intégratrice web : je recherche un stage ");
+})
