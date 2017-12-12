ba52c75070bccce7726ab64943cc6e40fb1d865e
babadam <barbara.tousverts@lepoles.com>
2017-12-12 17:41:06 +0100

    commit n°31

--- accueil_pro.php
+++ accueil_pro.php
@@ -6,7 +6,6 @@ $ligne_competence = $competences -> fetchAll(PDO::FETCH_ASSOC);
 // echo '<pre>';
 // print_r($ligne_competence);
 // echo '</pre>';
-
 ?>
 
 <!DOCTYPE html>
@@ -32,136 +31,137 @@ $ligne_competence = $competences -> fetchAll(PDO::FETCH_ASSOC);
           <button class="nav-button" role="button" type="button" aria-label="navigation"></button>
         </div>
 
-        <div id="slide1">
-            <div class="slide_inside">
-                <h1>Barbara Tousverts </h1>
-                <p class="typed"><span id="holder"></span><span class="blinking-cursor">|</span></p>
-            </div> <!--fin .slide_inside-->
-        </div> <!--fin #slide1-->
+        <main>
+            <div id="slide1">
+                <div class="slide_inside">
+                    <h1>Barbara Tousverts </h1>
+                    <p class="typed"><span id="holder"></span><span class="blinking-cursor">|</span></p>
+                </div> <!--fin .slide_inside-->
+            </div> <!--fin #slide1-->
 
-        <div id="slide2">
-            <div class="slide_inside">
-                <h2>Compétences</h2>
-
-                <div class="conteneur">
-                    <div class="competence">
-                        <div class="icone_competence">
-                            <span><i class="fa fa-code" aria-hidden="true"></i></span>
-                        </div>
-                        <div class="texte_competence">
-                            <h3>Intégration</h3>
-                            <?php
-                                for($i = 0; $i < count($ligne_competence); $i++ ){
-                                    // echo '<pre style="color: black; background-color:white;">';
-                                    // print_r($ligne_competence[$i]);
-                                    // echo '</pre>';
-                                    if($ligne_competence[$i]['categorie'] == 'dev_front'){
-                                        echo '<p>';
-                                        echo $ligne_competence[$i]['competence'];
-                                        echo '</p>';
+            <div id="slide2">
+                <div class="slide_inside">
+                    <h2>Compétences</h2>
+                    <div class="conteneur">
+                        <div class="competence">
+                            <div class="icone_competence">
+                                <span><i class="fa fa-code" aria-hidden="true"></i></span>
+                            </div>
+                            <div class="texte_competence">
+                                <h3>Intégration</h3>
+                                <?php
+                                    for($i = 0; $i < count($ligne_competence); $i++ ){
+                                        // echo '<pre style="color: black; background-color:white;">';
+                                        // print_r($ligne_competence[$i]);
+                                        // echo '</pre>';
+                                        if($ligne_competence[$i]['categorie'] == 'dev_front'){
+                                            echo '<p>';
+                                            echo $ligne_competence[$i]['competence'];
+                                            echo '</p>';
+                                        }
                                     }
-                                }
-                            ?>
+                                ?>
+                            </div>
                         </div>
-                    </div>
-                    <div class="competence">
-                        <div class="icone_competence">
-                            <span><i class="fa fa-cogs" aria-hidden="true"></i></span>
-                        </div>
-                        <div class="texte_competence">
-                            <h3>Développement</h3>
-                            <?php
-                                for($i = 0; $i < count($ligne_competence); $i++ ){
-                                    // echo '<pre style="color: black; background-color:white;">';
-                                    // print_r($ligne_competence[$i]);
-                                    // echo '</pre>';
-                                    if($ligne_competence[$i]['categorie'] == 'dev_back'){
-                                        echo '<p>';
-                                        echo $ligne_competence[$i]['competence'];
-                                        echo '</p>';
+                        <div class="competence">
+                            <div class="icone_competence">
+                                <span><i class="fa fa-cogs" aria-hidden="true"></i></span>
+                            </div>
+                            <div class="texte_competence">
+                                <h3>Développement</h3>
+                                <?php
+                                    for($i = 0; $i < count($ligne_competence); $i++ ){
+                                        // echo '<pre style="color: black; background-color:white;">';
+                                        // print_r($ligne_competence[$i]);
+                                        // echo '</pre>';
+                                        if($ligne_competence[$i]['categorie'] == 'dev_back'){
+                                            echo '<p>';
+                                            echo $ligne_competence[$i]['competence'];
+                                            echo '</p>';
+                                        }
                                     }
-                                }
-                            ?>
+                                ?>
+                            </div>
                         </div>
-                    </div>
-                    <div class="competence">
-                        <div class="icone_competence">
-                            <span><i class="fa fa-plus-square" aria-hidden="true"></i></span>
-                        </div>
-                        <div class="texte_competence">
-                            <h3>Framework</h3>
-                            <?php
-                                for($i = 0; $i < count($ligne_competence); $i++ ){
-                                    // echo '<pre style="color: black; background-color:white;">';
-                                    // print_r($ligne_competence[$i]);
-                                    // echo '</pre>';
-                                    if($ligne_competence[$i]['categorie'] == 'framework'){
-                                        echo '<p>';
-                                        echo $ligne_competence[$i]['competence'];
-                                        echo '</p>';
+                        <div class="competence">
+                            <div class="icone_competence">
+                                <span><i class="fa fa-plus-square" aria-hidden="true"></i></span>
+                            </div>
+                            <div class="texte_competence">
+                                <h3>Framework</h3>
+                                <?php
+                                    for($i = 0; $i < count($ligne_competence); $i++ ){
+                                        // echo '<pre style="color: black; background-color:white;">';
+                                        // print_r($ligne_competence[$i]);
+                                        // echo '</pre>';
+                                        if($ligne_competence[$i]['categorie'] == 'framework'){
+                                            echo '<p>';
+                                            echo $ligne_competence[$i]['competence'];
+                                            echo '</p>';
+                                        }
                                     }
-                                }
-                            ?>
+                                ?>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div> <!--.slide_inside-->
-        </div> <!--#slide2-->
+                </div> <!--.slide_inside-->
+            </div> <!--#slide2-->
 
-        <div id="slide3">
-            <div class="slide_inside">
-                <h2>Réalisations</h2>
-                <div class="polaroid-images">
-                    <a href="" title="Cave"><img height="200" src="img/site_cv.png" alt="Site CV" title="site cv" /></a>
-                    <a href="" title="Cave"><img height="200" src="img/site_html.png" alt="Site HTML" title="site html/css" /></a>
-                    <a href="" title="Cave"><img height="200" src="img/site_client.png" alt="Site client" title="site client" /></a>
-                </div>
-            </div> <!--.slide_inside-->
-        </div>
-        <div id="slide4">
-            <div class="slide_inside">
-                <h2>Expériences</h2>
-            </div> <!--.slide_inside-->
-        </div>
-        <div id="slide5">
-            <div class="slide_inside">
-                <h2>Contact</h2>
-                <div class="contact">
-                    <div class="info">
-                        <h3>Contactez-moi</h3>
-                        <div class="me">
-                            <i class="fa fa-map-marker" aria-hidden="true"></i>
-                            <p>Barbara Tousverts</p>
-                            <p>Colombes</p>
+            <div id="slide3">
+                <div class="slide_inside">
+                    <h2>Réalisations</h2>
+                    <div class="polaroid-images">
+                        <a href="" title="Cave"><img height="200" src="img/site_cv.png" alt="Site CV" title="site cv" /></a>
+                        <a href="" title="Cave"><img height="200" src="img/site_html.png" alt="Site HTML" title="site html/css" /></a>
+                        <a href="" title="Cave"><img height="200" src="img/site_client.png" alt="Site client" title="site client" /></a>
+                    </div>
+                </div> <!--.slide_inside-->
+            </div>
+            <div id="slide4">
+                <div class="slide_inside">
+                    <h2>Expériences</h2>
+                </div> <!--.slide_inside-->
+            </div>
+            <div id="slide5">
+                <div class="slide_inside">
+                    <h2>Contact</h2>
+                    <div class="contact">
+                        <div class="info">
+                            <h3>Contactez-moi</h3>
+                            <div class="me">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                <p>Barbara Tousverts</p>
+                                <p>Colombes</p>
+                            </div>
+                            <div class="phone">
+                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                <p>06.63.57.70.89</p>
+                            </div>
+                            <div class="email">
+                                <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                                <p>barbara.tousverts@live.fr</p>
+                            </div>
                         </div>
-                        <div class="phone">
-                            <i class="fa fa-phone" aria-hidden="true"></i>
-                            <p>06.63.57.70.89</p>
-                        </div>
-                        <div class="email">
-                            <i class="fa fa-envelope-o" aria-hidden="true"></i>
-                            <p>barbara.tousverts@live.fr</p>
+                        <div class="form-container">
+                            <form class="form-contact" action="" method="post">
+
+                                <input type="text" name="nom" id="nom" value="" placeholder="Nom"><br>
+
+                                <input type="email" name="email" id="email" value="" placeholder="Email"><br>
+
+                                <input type="text" name="phone" id="phone" value="" placeholder="Téléphone"><br>
+
+                                <input type="text" name="sujet" id="sujet" value="" placeholder="Sujet"><br>
+
+                                <textarea name="message" rows="8" cols="40" id=message placeholder="Message"></textarea>
+
+                                <input type="submit" name="" value="Envoyer">
+                            </form>
                         </div>
                     </div>
-                    <div class="form-container">
-                        <form class="form-contact" action="" method="post">
-
-                            <input type="text" name="nom" id="nom" value="" placeholder="Nom"><br>
-
-                            <input type="email" name="email" id="email" value="" placeholder="Email"><br>
-
-                            <input type="text" name="phone" id="phone" value="" placeholder="Téléphone"><br>
-
-                            <input type="text" name="sujet" id="sujet" value="" placeholder="Sujet"><br>
-
-                            <textarea name="message" rows="8" cols="40" id=message placeholder="Message"></textarea>
-
-                            <input type="submit" name="" value="Envoyer">
-                        </form>
-                    </div>
-                </div>
-            </div> <!--.slide_inside-->
-        </div>
+                </div> <!--.slide_inside-->
+            </div>
+        </main>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
         <script src="js/main.js" type="text/javascript"></script>
